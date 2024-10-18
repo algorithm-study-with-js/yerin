0ecb2c0a6794e0e5699c027253602efc8e92c300
annylee0723 <annylee0723@gmail.com>
2024-10-18 17:17:26 +0900

    fix: re-solve 12951

--- Greedy_Implementation/lv2/12951.js
+++ Greedy_Implementation/lv2/12951.js
@@ -1,8 +1,8 @@
 function solution(n) {
   const arr = n.split(' ');
-
+  //   console.log(arr);
   const answer = arr.map((word) => {
-    if (word.length == 0) return ' ';
+    if (word === '') return; // 이 부분 때문에 계속 오류 났었음,,,
 
     return word[0].toUpperCase() + word.slice(1).toLowerCase();
   });
@@ -10,4 +10,4 @@ function solution(n) {
   return answer.join(' ');
 }
 
-console.log(solution('hello  world'));
+console.log(solution(' hello  world'));
