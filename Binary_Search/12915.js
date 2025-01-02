bf9941080639191ba766ea7b80736d3b50d51899
annylee0723 <annylee0723@gmail.com>
2025-01-03 00:26:31 +0900

    feat:solve12915

---
+++ Binary_Search/12915.js
@@ -0,0 +1,11 @@
+function solution(strings, n) {
+  const answer = strings.sort((a, b) => {
+    if (a[n] === b[n]) {
+      return a < b ? -1 : a > b ? 1 : 0;
+    }
+    return a[n] < b[n] ? -1 : 1;
+  });
+  return answer;
+}
+
+console.log(solution(['sun', 'bed', 'car'], 1));
